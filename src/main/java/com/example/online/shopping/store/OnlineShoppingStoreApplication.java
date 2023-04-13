6e1a592553423cd999216efa4c94aaffe6e56afa
omer358 <omermaki358@gmail.com>
2023-04-13 20:31:40 +0200

    initialize project with dependencies

---
+++ src/main/java/com/example/online/shopping/store/OnlineShoppingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.online.shopping.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online/shopping/store/OnlineShoppingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online.shopping.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
